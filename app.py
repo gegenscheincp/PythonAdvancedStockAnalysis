c1bc3c25eadadd77db880df373c009d278ba604e
gegenscheincp <gegenscheincapitalpartners@gmail.com>
2026-01-15 16:07:16 -0500

    Refactor sys.path handling in app.py
    Removed redundant sys.path.append statement.

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import sys
 import os
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, ROOT_DIR)
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 import streamlit as st
 import plotly.express as px
 
